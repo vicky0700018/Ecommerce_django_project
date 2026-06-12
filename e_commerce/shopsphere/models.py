d172243c1eb45017cf716396b22b54d333932def
vicky0700018 <vs2734514@gmail.com>
2026-06-12 22:21:10 +0530

    updated

--- e_commerce/shopsphere/models.py
+++ e_commerce/shopsphere/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    )
+
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    mobile_no = models.CharField(max_length=15)
+    dob = models.DateField(null=True, blank=True)
+    address = models.TextField()
+    alternate_mobile_no = models.CharField(max_length=15, null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return self.email

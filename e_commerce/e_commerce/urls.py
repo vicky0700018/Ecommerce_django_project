881ab33fbd4d70547e60bbbfd8b28a998017f7a2
vicky0700018 <vs2734514@gmail.com>
2026-06-12 22:15:41 +0530

    updated

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from shopsphere.views import home
+from django.urls import path, include
+
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('', home),
+   path('', include('shopsphere.urls')),
 ]
 
 

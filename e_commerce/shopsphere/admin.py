7e6d2acdc42df61987b80468fa8270ecd3b4a982
vicky0700018 <vs2734514@gmail.com>
2026-06-14 17:53:11 +0530

    updated

--- e_commerce/shopsphere/admin.py
+++ e_commerce/shopsphere/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.utils.safestring import mark_safe
+from django.utils import timezone
+from .models import CustomUser, OTP
 
 class CustomUserAdmin(UserAdmin):
     # Completely redefine fieldsets to move 'email'
@@ -8,6 +10,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
         ('Custom Profile Details', {'fields': ('email', 'full_name', 'mobile_no', 'alternate_mobile_no', 'dob', 'address', 'profile_image', 'gender')}),
+        ('Email Verification', {'fields': ('is_email_verified',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
@@ -17,6 +20,63 @@ class CustomUserAdmin(UserAdmin):
         ('Custom Profile Details', {'fields': ('full_name', 'email', 'mobile_no', 'alternate_mobile_no', 'dob', 'address', 'profile_image', 'gender')}),
     )
     
-    list_display = ('username', 'email', 'full_name', 'mobile_no', 'is_staff')
+    list_display = ('username', 'email', 'full_name', 'mobile_no', 'is_staff', 'email_verification_status')
+    list_filter = ('is_email_verified', 'is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'email', 'full_name', 'mobile_no')
+    
+    def email_verification_status(self, obj):
+        """Display email verification status with color"""
+        if obj.is_email_verified:
+            return mark_safe(
+                '<span style="color: green; font-weight: bold;">✓ Verified</span>'
+            )
+        else:
+            return mark_safe(
+                '<span style="color: red; font-weight: bold;">✗ Not Verified</span>'
+            )
+    email_verification_status.short_description = 'Email Status'
+
+
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ('user_email', 'code', 'created_at', 'expires_at', 'is_verified', 'status')
+    list_filter = ('is_verified', 'created_at')
+    search_fields = ('user__email', 'code')
+    readonly_fields = ('code', 'created_at', 'expires_at')
+    
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user',)
+        }),
+        ('OTP Details', {
+            'fields': ('code', 'created_at', 'expires_at', 'is_verified')
+        }),
+    )
+    
+    def user_email(self, obj):
+        """Display user email"""
+        return obj.user.email
+    user_email.short_description = 'User Email'
+    
+    def status(self, obj):
+        """Display OTP status"""
+        if obj.is_verified:
+            return mark_safe(
+                '<span style="color: green; font-weight: bold;">✓ Verified</span>'
+            )
+        elif obj.is_expired():
+            return mark_safe(
+                '<span style="color: red; font-weight: bold;">✗ Expired</span>'
+            )
+        else:
+            return mark_safe(
+                '<span style="color: orange; font-weight: bold;">⏳ Pending</span>'
+            )
+    status.short_description = 'Status'
+    
+    def has_add_permission(self, request):
+        """Prevent manual OTP creation from admin"""
+        return False
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(OTP, OTPAdmin)

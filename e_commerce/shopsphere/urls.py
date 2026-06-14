fc1c896dfa1791d87c89a1ddbb81b49263747c26
vicky0700018 <vs2734514@gmail.com>
2026-06-14 17:53:45 +0530

    updated

--- e_commerce/shopsphere/urls.py
+++ e_commerce/shopsphere/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
 
    path('', home, name='home'),
    path('register/', register, name='register'),
+   path('verify-otp/', verify_otp, name='verify_otp'),
+   path('resend-otp/', resend_otp, name='resend_otp'),
    path('login/', login, name='login'),
    path('logout/', logout, name='logout'),
 ]

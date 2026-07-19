5b654db1aa8b8c43df046d56a39798e03d918f1d
vicky0700018 <vs2734514@gmail.com>
2026-07-19 13:56:14 +0530

    Fix static and media image loading on Vercel using WhiteNoise and vercel.json asset builds

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'e_commerce.urls'##Check Changes Project name
 
